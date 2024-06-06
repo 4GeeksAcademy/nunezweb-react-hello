48127d76d15900d864eae62adc0c750b1c95f314
Alejandro Núñez <145801334+nunezweb@users.noreply.github.com>
2024-06-06 08:25:53 +0000

    Fixed hamburger mobile menu

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,35 +2,43 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+    <nav className="navbar navbar-expand-md bg-dark navbar-dark">
       <div className="container">
-        <div className="navbar-header">
-          <a className="navbar-brand" href="#">
-            Start Bootstrap
-          </a>
+        <a className="navbar-brand" href="#">
+          Start Bootstrap
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="nav navbar-nav navbar-right">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Services
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Contact
-            </a>
-          </li>
-        </ul>
       </div>
     </nav>
   );
